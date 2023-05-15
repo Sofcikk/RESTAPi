11e6e0b7092a6ba58381ee317fa5e217f98bdc82
Zosia Samosia <Zosia@Samosia.pl>
2023-05-15 13:58:08 +0200

    refractor

--- js/main.js
+++ js/main.js
@@ -1,28 +1,55 @@
-'use strict';
+'use strict'
+const baseurl='https://api.nbp.pl/api/exchangerates/tables/'
 
-const getData = () => {
+const getCurrency=async (table) =>{
+    
+    try {
 
-    fetch('https://api.nbp.pl/api/exchangerates/tables/a/?format=json')
-    .then(response => response.json())
-    .then(result => {
-        console.log(result);
-        console.log(result[0].rates);
-        result[0].rates.forEach(element => {
-            const tr = document.createElement('tr')
-            tr.innerHTML = 
-            `<td>${++i}</td>
-            <td>${element.currenty}</td>
-            <td>${element.mid}</td>`
-            
-        });
-        document.querySelector('table tbody').appendChild(tr);
-    });
+        const Response = await fetch(`${baseurl}${table}`);
+        const data = await Response.json();
+        return data;
+    }catch(err) {
+        console.log(`To jest błąd : ${err}`);
+    }
 }
 
-const btn = document.querySelector('button')
+getCurrency('a').then(data => {
 
-btn.addEventListener('click', getData);
+    console.log(data);
+    //const curriencies = data[0]
+    //destrukturyzacja
+    const [currencies] = data;
 
-// for(let i = 0; ;i++) {
+    const {rates} = currencies;
 
-// }
+    rates.forEach((elem,i) => {
+        const tr = document.createElement('tr')
+            tr.innerHTML=`<td>${++i}</td>
+            <td>${elem.currency}</td>
+            <td>${elem.mid}</td>`
+            document.querySelector('table tbody').appendChild(tr)
+    })
+})
+const getData=()=>{
+
+    fetch(baseurl)
+    .then(Response => Response.json())
+    .then(result=> {
+        console.log(result)
+       
+        console.log(result[0].rates)
+        result[0].rates.forEach((elem, i) => {
+            const tr = document.createElement('tr')
+            tr.innerHTML=`<td>${++i}</td>
+            <td>${elem.currency}</td>
+            <td>${elem.mid}</td>`
+            document.querySelector('table tbody').appendChild(tr)
+        });
+    })
+}
+// git init, git add . , git commit -m"" ,
+// komedy do kita gałęzie
+// git branch (sprawdza)
+// git branch -d nazwa(usuwa)
+// git branch nazwa (dodaje)
+// git checkout -b dev(zmienia i tworzy)
